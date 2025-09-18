c3142fbba184c1eba2449465f8063ae8f34fe7a6
이재형 <wogud1514@rubric.im>
2025-09-18 14:48:38 +0900

    init

---
+++ src/main/java/rubric_labs/tts_project/TtsProjectApplication.java
@@ -0,0 +1,13 @@
+package rubric_labs.tts_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TtsProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TtsProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/rubric_labs/tts_project/TtsProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package rubric_labs.tts_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TtsProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
